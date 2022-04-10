1e14094a6e52dfc1037fcc0e4ffc9c4df77db61c
Agus <agustinsosabertolini@gmail.com>
2022-04-09 22:22:15 -0300

    Se agrega editar foto y banner

--- src/app/components/acerca/acerca.component.ts
+++ src/app/components/acerca/acerca.component.ts
@@ -15,6 +15,8 @@ export class AcercaComponent implements OnInit {
 
   acercas: Acerca[] = [new Acerca()];
   loading: boolean = false;
+  loadingFoto: boolean = true;
+  loadingBanner: boolean = true;
   error: boolean = false;
   fotoForm: FormGroup = this.formBuilder.group({
     foto: [''],
@@ -24,7 +26,10 @@ export class AcercaComponent implements OnInit {
   });
 
   editResumen: boolean = false;
-
+  editTitulo: boolean = false;
+  editNombre: boolean = false;
+  editFoto: boolean = false;
+  editBanner: boolean = false;
   fotoPath: SafeResourceUrl = "";
   bannerPath: SafeResourceUrl = "";
   constructor(
@@ -42,15 +47,14 @@ export class AcercaComponent implements OnInit {
   createFoto(image: Blob): any {
     if (image && image.size > 0) {
       let reader = new FileReader();
-
       reader.addEventListener("load", () => {
         this.fotoPath = this._sanitizer.bypassSecurityTrustResourceUrl(String(reader.result));
-        this.loading = false;
+        this.loadingFoto = false;
       }, false);
 
       reader.readAsDataURL(image);
     } else {
-      this.loading = false;
+      this.loadingFoto = false;
     }
   }
 
@@ -60,12 +64,11 @@ export class AcercaComponent implements OnInit {
 
       reader.addEventListener("load", () => {
         this.bannerPath = this._sanitizer.bypassSecurityTrustResourceUrl(String(reader.result));
-        this.loading = false;
+        this.loadingBanner = false;
       }, false);
-
       reader.readAsDataURL(image);
     } else {
-      this.loading = false;
+      this.loadingBanner = false;
     }
   }
 
@@ -91,6 +94,7 @@ export class AcercaComponent implements OnInit {
     this.acercaService.fetchImage(this.acercas[0].foto).subscribe({
       next: (image) => {
         this.createFoto(image);
+        this.loadingFoto = false;
       },
       error: (err) => {
         console.log(err);
@@ -102,6 +106,7 @@ export class AcercaComponent implements OnInit {
     this.acercaService.fetchImage(this.acercas[0].banner).subscribe({
       next: (image) => {
         this.createBanner(image);
+        this.loadingBanner = false;
       },
       error: (err) => {
         console.log(err);
@@ -109,16 +114,40 @@ export class AcercaComponent implements OnInit {
     });
   }
 
-  onFileSelect(event: any) {
+  onFileSelectFoto(event: any) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      this.fotoForm.get('imagen')!.setValue(file);
+      this.fotoForm.get('foto')!.setValue(file);
+    }
+  }
+
+  onFileSelectBanner(event: any) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.bannerForm.get('banner')!.setValue(file);
     }
   }
 
   toggleEditResumen(){
     this.editResumen = !this.editResumen;
   }
+
+  toggleEditTitulo(){
+    this.editTitulo = !this.editTitulo;
+  }
+
+  toggleEditNombre(){
+    this.editNombre = !this.editNombre;
+  }
+
+  toggleEditFoto(){
+    this.editFoto = !this.editFoto;
+  }
+
+  toggleEditBanner(){
+    this.editBanner = !this.editBanner;
+  }
+
 
   updateResumen(){
     this.acercaService.updateResumen(this.acercas[0].id, this.acercas[0].resumen).subscribe({
@@ -129,6 +158,76 @@ export class AcercaComponent implements OnInit {
       },
       error: () => {
         this.toastr.error('Error al actualizar el resumen', 'Error');
+      }
+    });
+  }
+
+  updateTitulo(){
+    this.acercaService.updateTitulo(this.acercas[0].id, this.acercas[0].titulo).subscribe({
+      next: (data: any) => {
+        this.getAll();
+        this.toastr.success('Titulo actualizado', 'Éxito');
+        this.toggleEditTitulo();
+      },
+      error: () => {
+        this.toastr.error('Error al actualizar el titulo', 'Error');
+      }
+    });
+  }
+
+  updateNombre(){
+    this.acercaService.updateNombre(this.acercas[0].id, this.acercas[0].nombre).subscribe({
+      next: (data: any) => {
+        this.getAll();
+        this.toastr.success('Nombre actualizado', 'Éxito');
+        this.toggleEditNombre();
+      },
+      error: () => {
+        this.toastr.error('Error al actualizar el nombre', 'Error');
+      }
+    });
+  }
+
+  updateFoto() {
+    const formData = new FormData();
+    const formulario = this.fotoForm.value;
+    Object.keys(formulario).forEach(key => {
+      formData.append(key, formulario[key]);
+    });
+
+    this.acercaService.updateFoto(this.acercas[0].id, formData).subscribe({
+      next: (data: any) => {
+        this.toastr.success('Foto actualizada', 'Éxito');
+        this.toggleEditFoto()
+        this.fotoForm = this.formBuilder.group({
+          foto: [''],
+        });
+        this.getAll();
+      },
+      error: (err: any) => {
+        this.toastr.error('Error al actualizar la foto', 'Error');
+      }
+    });
+  }
+
+  updateBanner() {
+    const formData = new FormData();
+    const formulario = this.bannerForm.value;
+    Object.keys(formulario).forEach(key => {
+      formData.append(key, formulario[key]);
+    });
+
+    this.acercaService.updateBanner(this.acercas[0].id, formData).subscribe({
+      next: (data: any) => {
+        this.toastr.success('Banner actualizado', 'Éxito');
+        this.toggleEditBanner()
+        this.bannerForm = this.formBuilder.group({
+          banner: [''],
+        });
+        this.getAll();
+      },
+      error: (err: any) => {
+        this.toastr.error('Error al actualizar el banner', 'Error');
       }
     });
   }
